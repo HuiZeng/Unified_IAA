99ea61c593d68292c5d9dd586d3282ada4f082e9
Hui Zeng <HuiZeng@users.noreply.github.com>
2018-03-08 14:23:23 +0800

    Add files via upload

---
+++ RunExperiment.m
@@ -0,0 +1,22 @@
+%  please replace "tools/matconvnet/matlab/+dagnn/Loss.m" with "tools/src/Loss.m" before run ResNet
+
+% clc;
+clear;
+warning off
+addpath(genpath('tools'));
+vl_setupnn;
+
+testDatabase = 'AVA'; % AADB
+Model = 'resnet50'; % resnet50  vgg16
+epoch = 10;
+fixImgSize = 384;
+
+lossType = 'CE'; % MSE, Huber, CE
+labelType = 'Gauss_OV'; % Gauss_OV Gauss_MV RSD binaryLabel qualityScore
+
+trainModel('Model',Model,'database',testDatabase,'lossType',lossType,...
+           'labelType',labelType,'epoch',epoch,'fixImgSize',fixImgSize);
+
+
+
+
